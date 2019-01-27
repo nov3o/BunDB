43f2c21f512f4b62c6c9536b243fada5ffb8a165
nov3o <ilya.bad111@ya.ru>
2019-01-27 21:15:53 +0300

    Added first table load

--- dbms.cpp
+++ dbms.cpp
@@ -5,6 +5,14 @@ using namespace std;
 
 int main()
 {
+	// First db load for test
+/*	Person table[] = {
+		{"John", "Doe", 1, 20},
+		{"Johana", "Doney", 0, 22}
+	};
+	fstream file("table", ios::binary|ios::out|ios::trunc);
+	file.write((char*)table, sizeof(table));
+	*/
 	Table table("table");
 	cout << table;
 	delete [] table.rows;
